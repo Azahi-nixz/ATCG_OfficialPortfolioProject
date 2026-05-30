720f87bd5b4dc38aa42b952e253544587a049552
Azahi_nixz <delcorodwight@gmail.com>
2026-05-30 19:12:07 +0800

    Deploy: added full TCG-React project files to repository

---
+++ TCG-React/src/Login.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import "./login.css";
+import img1 from "./assets/img1.jpeg";
+import img2 from "./assets/img2.jpg";
+import img4 from "./assets/img4.png";
+import img5 from "./assets/img5.jpg";
+import logo from "./assets/logo.png";
+import alya from "./assets/alya.png"
+
+
+
+function Login() {
+  useEffect(() => {
+    document.title = "Log-in";
+  }, []);
+
+  const imgArr = [img1, img2, img4, img5];
+  const colorArr = ["pink" , "red" , "cyan" , "brown"]
+
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+
+  useEffect(() => {
+    const imageTimer = setInterval(() => {
+      setCurrentImgIndex((prevIndex) => {
+        if (prevIndex === imgArr.length - 1) {
+          return 0;
+        } else {
+          return prevIndex + 1;
+        }
+      });
+    }, 10000);
+
+    return () => clearInterval(imageTimer);
+  }, [imgArr.length]);
+
+  const displayImg = imgArr[currentImgIndex];
+  const color = colorArr[currentImgIndex];
+    
+  return (
+    
+    <div 
+      className="Login" 
+      style={{ backgroundImage: `url(${displayImg})`, 
+        backgroundSize: '90%',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundColor: color}}
+    > 
+    <div className="pic">
+      <img src={logo} alt="Logo" className="logo"/>
+    </div>
+    
+      <div className="box">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <h1 className="Log-in">
+            ♡ Log-in ♡
+          </h1>
+          <hr className="line" />
+          
+          <div className="Email">
+            <label htmlFor="email" className="email">Email</label>
+            <input type="email" id="email" className="em" placeholder="E-mail" />
+          </div>
+          
+          <div className="Password">
+            <label htmlFor="password" className="password">Password</label>
+            <input type="password" id="password" className="pw" placeholder="Password" />
+          </div>
+          
+          <button type="submit" className="Submit">
+            Submit
+          </button>
+
+          <div className="reset">
+            <a href="#" className="rp">Forgot password? Click here!</a>
+          </div>
+
+          <hr className="line" />
+
+          <div className="Signup">
+            <p className="sign">
+              Don't have an account yet? <span className="sp"><a href="#">Sign up here!</a></span>
+            </p>
+          </div>
+        </form>
+      </div>
+
+       <div className="alya">
+      <img src={alya} alt="Logo" className="logo"/>
+      </div>
+
+
+    <div className="ft">
+      <footer className="footer">
+        <h1 className="greet">Welcome to ATCG!</h1>
+        <hr className="foot"></hr>
+        <p className="intro"> This website will collect some information including but not limited to your cookies.
+          By logging in or signing-up, you acknowledge and entrust your personal information to the developer of ATCG.
+          Thanks for playing!
+        </p>
+      </footer>
+    </div>
+    </div>
+  );
+}
+
+export default Login;
