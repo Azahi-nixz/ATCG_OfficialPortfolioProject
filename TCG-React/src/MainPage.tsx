6e99ac910c9aab9de24bf7ac5d48387debca3296
Azahi_nixz <delcorodwight@gmail.com>
2026-06-01 17:51:14 +0800

    Fixed some bugs, enhanced sidebar UI

--- TCG-React/src/MainPage.tsx
+++ TCG-React/src/MainPage.tsx
@@ -1,13 +1,6 @@
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { useState } from "react";
 import "./login.css";
 import { Link } from "react-router-dom";
-import img1 from "./assets/img1.jpeg";
-import img2 from "./assets/img2.jpg";
-import img4 from "./assets/img4.png";
-import img5 from "./assets/img5.jpg";
-import logo from "./assets/logo.png";
-import alya from "./assets/alya.png"
 import pfp from "./assets/pfp.jpg"
 
 
@@ -31,7 +24,7 @@ function MainPage() {
             
             <div className="nav-page">
                 <div className="profile">
-                    <img src={pfp} alt="Logo" className="logo" />
+                    <img src={pfp} alt="Logo" className="pf" />
                     <h2 className="username">Azahi</h2>
                     <button className="back" onClick={toggleSidebar}>
                         →
